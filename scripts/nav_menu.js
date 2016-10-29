ea9e315680405f00b981499936e9adeb477aaab6
piyush-jaiswal <piyush.jaiswal@st.niituniversity.in>
2016-10-29 23:56:08 +0530

    improved functionality of nav-menu

--- scripts/nav_menu.js
+++ scripts/nav_menu.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-  $('.wrapper').click(function() {
+  $('.wrapper').click(function(event) {
+    event.stopPropagation();
       $('.bars').toggleClass('toggle');
       $('.main-nav').toggleClass('expand');
       $('.main-nav ul').toggleClass('hide');
     });
 
-  $('.menu-items a').click(function() {
+  /*$('.menu-items a').click(function() {
       $('.bars').toggleClass('toggle');
       $('.main-nav').toggleClass('expand');
       $('.main-nav ul').toggleClass('hide');
-    });
+    }); */
 
+//For scroll animation
   $('.menu-items a').on('click', function(e) {
     e.preventDefault();
     var section = $(this).attr('href').substr($(this).attr('href').indexOf('#'));
@@ -20,8 +22,21 @@ $(document).ready(function() {
       scrollTop: $section.offset().top + 'px'
     }, 900);
   });
+  
+  //For clicking after nav-menu is displayed
+  $(window).click(function(e) {
 
+     if($('.main-nav').hasClass('expand')) {
+       $('.bars').toggleClass('toggle');
+       $('.main-nav').toggleClass('expand');
+       $('.main-nav ul').toggleClass('hide');
+     }
+
+  });
+ 
 
 });
+
+ 
 
 
