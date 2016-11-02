f38efb1d01d0acc1acc031edd3a0b6a12c15785d
Karanjit Singh <ksg207@live.com>
2016-11-02 23:36:00 +0530

    Autoplay delay added

---
+++ scripts/autoplay.js
@@ -0,0 +1,18 @@
+
+setTimeout(function(){
+    document.getElementById("teaser_video").play();
+}, 2000);
+
+/*
+var vid = document.getElementById("teaser_video");
+
+function enableAutoplay() { 
+    vid.autoplay = false;
+
+    setTimeout(function(){
+        vid.autoplay = true;
+    }, 5000);
+
+    vid.load();
+}
+*/
