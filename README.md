4519e4df474def19f9d58af397608b19c8e25e1b
Karanjit Singh <ksg207@live.com>
2016-11-10 20:57:15 +0530

    merge conflict resolved

---
+++ README.md
@@ -0,0 +1,4 @@
+# ingenuity17
+
+Agenda : Build a Landing Page for IngeNUity 17
+
---
+++ scripts/autoplay.js
@@ -0,0 +1,20 @@
+
+
+setTimeout(function(){
+    document.getElementById("teaser_video").play();
+}, 3000);
+/*
+
+var vid = document.getElementById("teaser_video");
+vid.pause();
+
+/*function enableAutoplay() { 
+    vid.autoplay = false;
+
+    setTimeout(function(){
+        vid.autoplay = true;
+    }, 5000);
+
+    vid.load();
+}
+*/
